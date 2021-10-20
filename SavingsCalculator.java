048c11b1010d2261fa9063be298319c30f0e90c3
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-10-20 09:29:17 -0400

    5B
    Added Scanner Code

--- SavingsCalculator.java
+++ SavingsCalculator.java
@@ -3,7 +3,17 @@
 public class SavingsCalculator {
     
     public static void main(String[] args) { 
-        System.out.println("Please enter your account number:");
-        
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please enter your 12-Digit account_number:");
+
+        String account_number = myScanner.nextLine(); 
+
+        System.out.println("Your Account number is:" + account_number);
+
+        int myInt = myScanner.nextInt();
+        double myDouble = myScanner.nextDouble();
+
+
     }
 }
