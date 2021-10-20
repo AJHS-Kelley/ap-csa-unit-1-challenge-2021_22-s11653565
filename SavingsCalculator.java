d03d84ae62689b5e0dd9540631b1680b0eba01cc
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-10-20 09:07:32 -0400

    5B
    Added ask for 12 digit account number

---
+++ SavingsCalculator.java
@@ -0,0 +1,9 @@
+// <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:43>, v0.0
+
+public class SavingsCalculator {
+    
+    public static void main(String[] args) { 
+        System.out.println("Please enter your account number:");
+        
+    }
+}
