78467cdd285cafd593cc592123db82538be7ce2b
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-10-22 09:34:53 -0400

    5B
    finished scanner code and added account name

--- SavingsCalculator.java
+++ SavingsCalculator.java
@@ -1,18 +1,23 @@
-// <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:43>, v0.05
-// Peer Debugging by William Byson
+// <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:43>, v0.056
+import java.util.Scanner; 
+
+
 public class SavingsCalculator {
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Please enter your 12-Digit account_number:");
+        System.out.println("Please enter your 12-Digit account number:");
 
-        String account_number = myScanner.nextLine();
+        String accountNumber = myScanner.nextLine();
 
-        System.out.println("Your Account number is:" + account_number);
+        System.out.println("Your Account number is:" + accountNumber);
+
+        System.out.println("Enter your account name: ");
 
         int myInt = myScanner.nextInt();
         double myDouble = myScanner.nextDouble();
-
+        
+        
     }
 }
