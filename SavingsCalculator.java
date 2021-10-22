f31f51cb92850e8d66d6664795df71d6affd85b7
willthegrea <70521855+willthegrea@users.noreply.github.com>
2021-10-22 08:34:34 -0400

    5b
    peer debugging

--- SavingsCalculator.java
+++ SavingsCalculator.java
@@ -1,19 +1,18 @@
 // <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:43>, v0.05
-
+// Peer Debugging by William Byson
 public class SavingsCalculator {
-    
-    public static void main(String[] args) { 
+
+    public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Please enter your 12-Digit account_number:");
 
-        String account_number = myScanner.nextLine(); 
+        String account_number = myScanner.nextLine();
 
         System.out.println("Your Account number is:" + account_number);
 
         int myInt = myScanner.nextInt();
         double myDouble = myScanner.nextDouble();
-        
 
     }
 }
