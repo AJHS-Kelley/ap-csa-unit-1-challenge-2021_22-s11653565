438222c2c0548433133ef253671b5032a61f7554
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-10-28 09:38:48 -0400

    5B

--- SavingsCalculator.java
+++ SavingsCalculator.java
@@ -1,4 +1,4 @@
-// <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/28/2021>, <9:00>, v0.065
+// <Mauricio Salmoran>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/28/2021>, <9:39>, v0.07
 import java.util.Scanner; 
 
 
@@ -18,14 +18,17 @@ public class SavingsCalculator {
         
         System.out.print("Enter the opening balance for Account #" + accountNumber);
         System.out.println(".  Do not include $ or , please:");
-        int myInt = myScanner.nextInt();
+        
+        double myopeningBalance = myScanner.nextDouble();
+        String openingBalance = myScanner.nextLine();
         
         System.out.print("Enter the current interest rate in percentage format.");
         System.out.println(".  Do not include the % please:");
 
-
+        
         double myDouble = myScanner.nextDouble();
-        
-        
+       
+        String interestRate = myScanner.nextLine();
+        Interest *= openingBalance *= interestRate *= .12
     }
 }
